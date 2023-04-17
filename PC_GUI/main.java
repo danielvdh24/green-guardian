1fdbeccd1c3efa47cc6bb0f0586b36d53ec4f95d
Danone616 <danylo.akulich@gmail.com>
2023-04-17 11:56:23 +0200

    Develop code to display sensor data #18

--- PC_GUI/main.java
+++ PC_GUI/main.java
@@ -1,10 +1,10 @@
 package PC_GUI;
 
-
 class Main
 {
     public static void main(String[] args) 
-    {
-        System.out.print("test");
+    {   
+        Reader reader = new Reader("Test_data\\test1.txt");
+        for(int i=0;i<30;i++)System.out.println(reader.nextInt());
     }
 }
