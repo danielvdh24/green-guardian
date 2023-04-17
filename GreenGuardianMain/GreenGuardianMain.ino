f9482107e1adb501f11769eb349e3cea54842005
Markus <warbert@student.chalmers.se>
2023-04-17 15:30:50 +0200

    Receive data from GROVE Light Sensor #3

--- GreenGuardianMain/GreenGuardianMain.ino
+++ GreenGuardianMain/GreenGuardianMain.ino
@@ -3,9 +3,9 @@ const int lightPin = A1;
 const int temperaturePin = A2;
 
 void setup(){
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop(){
-  // put your main code here, to run repeatedly:
+  int lightLevel = analogRead(lightPin);
 }
