c8eddf2677be0d184c63294821352d0cbc24825f
Daniel Van Den Heuvel <heuvel@student.chalmers.se>
2023-04-19 14:31:42 +0200

    Develop a data processing algorithm for the GROVE Moisture Sensor #7
    Programmed Button_3 of the Wio Seeed Terminal to act as a toggle to switch between showing light values and moisture values. Created the testMoisture method with ranges taken from the sensor's documentation which indicates the soil's current moisture in 3 levels: Dry, Moist, and Wet. Further testing will have to be done to see if these values are accurate, preferably with actual soil rather than just wet tissues.
    
    Co-Authored-By: @manea <gusmanevl@student.gu.se>

--- GreenGuardianMain/GreenGuardianMain.ino
+++ GreenGuardianMain/GreenGuardianMain.ino
@@ -1,36 +1,45 @@
+// below is the setup needed for the RGB light stick library
 #include "Adafruit_NeoPixel.h"
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 #define PIN            BCM3
 #define NUMPIXELS      10
-
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 const int moisturePin = A0;
 const int temperaturePin = A1;
+bool isTestLight = true;
 
 void setup(){
   pinMode(moisturePin, INPUT);
   pinMode(temperaturePin, INPUT);
-  pinMode(WIO_LIGHT, INPUT); 
-  pixels.setBrightness(50);
-  pixels.begin(); 
+  pinMode(WIO_LIGHT, INPUT);
+  pinMode(BUTTON_3, INPUT_PULLUP);
+  pixels.setBrightness(50);           // brightness of led stick
+  pixels.begin();
   Serial.begin(9600);
 }
 
 void loop(){
   int moistureLevel = analogRead(moisturePin);
   int temperatureLevel = analogRead(temperaturePin);
-  int lightLevel = analogRead(WIO_LIGHT); 
-  Serial.println(lightLevel);
-  testLight(lightLevel);
+  int lightLevel = analogRead(WIO_LIGHT);
+  if (isTestLight) {
+    testLight(lightLevel);
+  } else {
+    testMoisture(moistureLevel);
+  }
+  if (digitalRead(BUTTON_3) == LOW) {
+    delay(200);
+    isTestLight = !isTestLight; // toggle to change mode of RGB stick
+    delay(200);
+  }
 }
 
 void testLight(int lightLevel){
  pixels.clear();
- int range = map(lightLevel, 0, 1300, 0, 10);
- Serial.println(range);
+ int range = map(lightLevel, 0, 1300, 0, 10);         // map light values to a range to activate leds
  if(range < 3 || range > 8){
    for(int i = 0; i < range || (i == 0 && range == 0); i++){
     pixels.setPixelColor(i, pixels.Color(255,0,0));   // red for too low/high
@@ -43,4 +52,24 @@ void testLight(int lightLevel){
  pixels.show();
 }
 
-
+void testMoisture(int moistureLevel){
+ pixels.clear();
+ int range;
+  if (moistureLevel >= 0 && moistureLevel < 300) {           // dry - brown
+    range = map(moistureLevel, 0, 299, 0, 3);
+    for(int i = 0; i < range || (i == 0 && range == 0); i++){
+     pixels.setPixelColor(i, pixels.Color(255,0,0));
+    }
+  } else if (moistureLevel >= 300 && moistureLevel < 600) {  // moist - light blue
+    range = map(moistureLevel, 300, 599, 3, 7);
+    for(int i = 0; i < range; i++){
+     pixels.setPixelColor(i, pixels.Color(69,165,217));
+    }
+  } else {                                                   // wet - dark blue
+    range = map(moistureLevel, 600, 1023, 7, 10);
+    for(int i = 0; i < range; i++){
+     pixels.setPixelColor(i, pixels.Color(0,0,255));
+    }
+  }
+ pixels.show();
+}
