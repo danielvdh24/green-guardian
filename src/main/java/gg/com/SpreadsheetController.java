d65bb27b8ab66de3cf7ca1552dcd30b2cf770ceb
Danone616 <danylo.akulich@gmail.com>
2023-05-01 14:53:44 +0200

    Develop code to display sensor data #18. Ensure that the data is easily understandable and useful for users #19

--- src/main/java/gg/com/SpreadsheetController.java
+++ src/main/java/gg/com/SpreadsheetController.java
@@ -7,46 +7,111 @@ import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.beans.value.*;
-import javafx.beans.property.*;
 import javafx.util.*;
+import javafx.scene.control.Button;
 
 public class SpreadsheetController 
 {
 
     @FXML
-    
-    TableView<TableEntry> Table;
-    TableColumn<TableEntry,Integer> PlantID;
-    TableColumn<TableEntry,String> Name;
-    TableColumn<TableEntry,Integer> Current_Temperature;
-    TableColumn<TableEntry,Integer> Current_Humidity;
-    TableColumn<TableEntry,Integer> Current_Light;
-    TableColumn<TableEntry,Integer> Optimal_Temperature;
-    TableColumn<TableEntry,Integer> Optimal_Humidity;
-    TableColumn<TableEntry,Integer> Optimal_Light;
-    TableColumn<TableEntry,String> Status;
-    public void initialize() {
-        ObservableList<TableEntry> data = FXCollections.observableArrayList();
+    ObservableList<TableEntry> data = FXCollections.observableArrayList();
+    public Button Commands;
+    public TableView<TableEntry> Table;
+    public TableColumn<TableEntry,String> PlantID;
+    public TableColumn<TableEntry,String> Name;
+    public TableColumn<TableEntry,String> Current_Temperature;
+    public TableColumn<TableEntry,String> Current_Humidity;
+    public TableColumn<TableEntry,String> Current_Light;
+    public TableColumn<TableEntry,String> Optimal_Temperature;
+    public TableColumn<TableEntry,String> Optimal_Humidity;
+    public TableColumn<TableEntry,String> Optimal_Light;
+    public TableColumn<TableEntry,String> Status;
+    public void initialize() 
+    {
         data.add(new TableEntry(0, "1", 0, 0, 0, 0, 0, 0, "1"));
-        Table = new TableView<>(data);
-        PlantID = new TableColumn<>("Plant ID");
-        Name = new TableColumn<TableEntry,String>("Name");
-        Current_Temperature = new TableColumn<>("Temperature");
-        Current_Humidity = new TableColumn<>("Humidity");
-        Current_Light = new TableColumn<>("Light");
-        Optimal_Temperature = new TableColumn<>("Temperature");
-        Optimal_Humidity = new TableColumn<>("Humidity");
-        Optimal_Light = new TableColumn<>("Light");
-        Status = new TableColumn<>("Status");
+        Table.setItems(data);
 
-        /// TO BE IMPLEMENTED 
+        
+        PlantID.setCellValueFactory
+            (
+            new Callback<CellDataFeatures<TableEntry, String>, ObservableValue<String>>() 
+                {
+                    public ObservableValue<String> call(CellDataFeatures<TableEntry, String> p) {return p.getValue().IDProperty();}
+                }
+            );
+        
+        Name.setCellValueFactory
+            (
+            new Callback<CellDataFeatures<TableEntry, String>, ObservableValue<String>>() 
+                {
+                    public ObservableValue<String> call(CellDataFeatures<TableEntry, String> p) {return p.getValue().nameProperty();}
+                }
+            );
 
-        /*PlantID.setCellValueFactory(new Callback<CellDataFeatures<TableEntry, Integer>, ObservableValue<Integer>>() {
-            public ObservableValue<Integer> call(CellDataFeatures<TableEntry, Integer> p) 
-            {
-                // p.getValue() returns the Person instance for a particular TableView row
-                return p.getValue().IDProperty();
-            }
-        })*/
+        Current_Temperature.setCellValueFactory
+            (
+            new Callback<CellDataFeatures<TableEntry, String>, ObservableValue<String>>() 
+                {
+                    public ObservableValue<String> call(CellDataFeatures<TableEntry, String> p) {return p.getValue().CTProperty();}
+                }
+            );
+        
+        Current_Humidity.setCellValueFactory
+            (
+            new Callback<CellDataFeatures<TableEntry, String>, ObservableValue<String>>() 
+                {
+                    public ObservableValue<String> call(CellDataFeatures<TableEntry, String> p) {return p.getValue().CHProperty();}
+                }
+            );
+
+        Current_Light.setCellValueFactory
+            (
+            new Callback<CellDataFeatures<TableEntry, String>, ObservableValue<String>>() 
+                {
+                    public ObservableValue<String> call(CellDataFeatures<TableEntry, String> p) {return p.getValue().CLProperty();}
+                }
+            );
+        
+        Optimal_Temperature.setCellValueFactory
+            (
+            new Callback<CellDataFeatures<TableEntry, String>, ObservableValue<String>>() 
+                {
+                    public ObservableValue<String> call(CellDataFeatures<TableEntry, String> p) {return p.getValue().OTProperty();}
+                }
+            );
+
+        Optimal_Humidity.setCellValueFactory
+            (
+            new Callback<CellDataFeatures<TableEntry, String>, ObservableValue<String>>() 
+                {
+                    public ObservableValue<String> call(CellDataFeatures<TableEntry, String> p) {return p.getValue().OHProperty();}
+                }
+            );
+        
+        Optimal_Light.setCellValueFactory
+            (
+            new Callback<CellDataFeatures<TableEntry, String>, ObservableValue<String>>() 
+                {
+                    public ObservableValue<String> call(CellDataFeatures<TableEntry, String> p) {return p.getValue().OLProperty();}
+                }
+            );
+        
+        Status.setCellValueFactory
+            (
+            new Callback<CellDataFeatures<TableEntry, String>, ObservableValue<String>>() 
+                {
+                    public ObservableValue<String> call(CellDataFeatures<TableEntry, String> p) {return p.getValue().statusProperty();}
+                }
+            );
+        
     }
+    void test()
+    {
+        int i=0;
+        while(true)
+        {data.get(0).ID.setText((Integer.toString(i)));
+        i++;
+        }
+    }
+
 }
