74023bf669519d01d05cd4d9d10de42fb1fe6e0e
Danone616 <danylo.akulich@gmail.com>
2023-05-16 12:36:48 +0200

    #17 : On the PC app, create a panel to visualize sensor data. #18 : Develop code to display sensor data.

--- src/main/java/gg/com/GraphController.java
+++ src/main/java/gg/com/GraphController.java
@@ -1,7 +1,11 @@
 package gg.com;
 
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.paint.Color;
 import javafx.animation.PauseTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
@@ -11,38 +15,60 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class GraphController {
+    private final String[] sources = {"Moisture","Temperature","Light"};
     @FXML
     private LineChart<String, Number> chart;
     @FXML
     private NumberAxis yAxis;
-
+    @FXML   
+    private ChoiceBox<String> dataSelect;
     @FXML
     public void initialize(){
+        dataSelect.getItems().addAll(sources);
+        dataSelect.setOnAction(this::SwitchData);
+        dataSelect.setValue(sources[0]);
         yAxis.setLowerBound(0);
-        yAxis.setUpperBound(300);
+        yAxis.setUpperBound(100);
         launchGraph();
     }
 
     @FXML
     protected void launchGraph() {
+        chart.getData().clear();
         XYChart.Series<String, Number> series = new XYChart.Series<>();
         Reader reader = new Reader(Objects.requireNonNull(App.class.getClassLoader().getResource("test.txt")).getFile());
+        if(dataSelect.getValue().equals("Light"))
+        {
+            reader.nextInt();
+            reader.nextInt();
+        }
+        if(dataSelect.getValue().equals("Temperature"))
+        {
+            reader.nextInt();
+        }
         int count = 1;
         chart.getData().add(series);
         addDataWithDelay(series, reader, count);
     }
 
     private void addDataWithDelay(XYChart.Series<String, Number> series, Reader reader, int count) {
-        if (count > 100) { // temporary limit
+        if (count > 335) { // temporary limit
             return;
         }
         String count1 = Integer.toString(count);
-        PauseTransition pause = new PauseTransition(Duration.seconds(SettingsController.getDelay()));
+        PauseTransition pause = new PauseTransition(Duration.seconds(0));
         pause.setOnFinished(event -> {
             series.getData().add(new XYChart.Data<>(count1, reader.nextInt()));
+            reader.nextInt();
+            reader.nextInt();
             addDataWithDelay(series, reader, count + 1);
         });
         pause.play();
+    }
+
+    private void SwitchData(ActionEvent event)
+    {
+        launchGraph();
     }
 
     public void onSpreadSheetButtonClick() throws IOException {
