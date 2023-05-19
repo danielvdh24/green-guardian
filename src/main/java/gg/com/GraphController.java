a2df2413612ba169314d447a039debde4647ae61
Danone616 <danylo.akulich@gmail.com>
2023-05-19 12:32:07 +0200

    #17 : On the PC app, create a panel to visualize sensor data. #18 : Develop code to display sensor data.

--- src/main/java/gg/com/GraphController.java
+++ src/main/java/gg/com/GraphController.java
@@ -10,6 +10,9 @@ import javafx.scene.chart.XYChart;
 import javafx.util.Duration;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class GraphController {
     private final String[] sources = {"Moisture","Temperature","Light"};
@@ -21,6 +24,18 @@ public class GraphController {
     private ChoiceBox<String> dataSelect;
     @FXML
     public void initialize(){
+        Path dataFilePath = Paths.get("config", "GraphData.txt");
+        try
+        {
+            if (!Files.exists(dataFilePath)) 
+            {
+            Files.createFile(dataFilePath);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Folder not found.");
+        }
         dataSelect.getItems().addAll(sources);
         dataSelect.setOnAction(this::SwitchData);
         dataSelect.setValue(sources[0]);
@@ -33,7 +48,7 @@ public class GraphController {
     protected void launchGraph() {
         chart.getData().clear();
         XYChart.Series<String, Number> series = new XYChart.Series<>();
-        Reader reader = new Reader("test.txt");
+        Reader reader = new Reader("GraphData.txt");
         if(dataSelect.getValue().equals("Light"))
         {
             reader.nextInt();
