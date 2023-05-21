29b8a42a5549ed222359f907da4c218e7d18ec99
Kai Rowley <rowley@student.gu.se>
2023-05-21 12:45:25 +0200

    Implement data formatting for transmission to PC #14

--- src/main/java/gg/com/WioPubDisconnector.java
+++ src/main/java/gg/com/WioPubDisconnector.java
@@ -1,24 +1,20 @@
 package gg.com;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
-
 public class WioPubDisconnector extends Thread {
     private MqttController mqttController;
     private MqttClient mqttClient;
-
     WioPubDisconnector(MqttController mqttController, MqttClient mqttClient) {
         this.mqttController = mqttController;
         this.mqttClient = mqttClient;
     }
-
     @Override
     public void run() {
+
         try {
-            mqttController.publish("stoppub;");
+            mqttController.publish("stoppub;", true);
             mqttClient.disconnect();
             mqttClient.close();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        } catch (Exception e){}
     }
 }
