39b21adaba54a7d2a91d99f96f967a4140ef2862
salmanb <gusbabarsa@student.gu.se>
2023-05-21 16:48:30 +0200

    changed WiFi and MQTT connectivity screen fonts on Wio terminal for improved UI #57

--- GreenGuardianMain/GreenGuardianMain.ino
+++ GreenGuardianMain/GreenGuardianMain.ino
@@ -236,24 +236,24 @@ void toggleOffline(){
 
 void connectWifi(){
 
-  displayLCDmessage("(Re) Connecting To WiFi...", tft.color565(20, 70, 150), FM9, true, true, 50);
-  displayLCDmessage("(Connection Keeps Failing? Fix:", tft.color565(70, 50, 50), FF25, true, false);
-  displayLCDmessage("1. WiFi Turned Off", tft.color565(70, 50, 50), FF25, true, false);
-  displayLCDmessage("2. Missing Code WiFi Config", tft.color565(70, 50, 50), FF25, true, false);
-  displayLCDmessage("3. Faulty AP Point Config)", tft.color565(70, 50, 50), FF25, true, false);
-  displayLCDmessage("Alt 2 & 3. Needs Manual Wio Restart", TFT_BLACK, FF33, true, false);
-  displayLCDmessage("After Reconfiguration", TFT_BLACK, FF33, true, false);
+  displayLCDmessage("(Re) Connecting To WiFi...", tft.color565(0, 0, 0), FSSB9, true, true, 50);
+  displayLCDmessage("(Connection Keeps Failing? Fix:", tft.color565(0, 0, 0), FSS9, true, false);
+  displayLCDmessage("1. WiFi Turned Off", tft.color565(0, 0, 0), FSS9, true, false);
+  displayLCDmessage("2. Missing Code WiFi Config", tft.color565(0, 0, 0), FSS9, true, false);
+  displayLCDmessage("3. Faulty AP Point Config)", tft.color565(0, 0, 0), FSS9, true, false);
+  displayLCDmessage("Alt 2 & 3. Needs Manual Wio Restart", TFT_BLACK, FSS9, true, false);
+  displayLCDmessage("After Reconfiguration", TFT_BLACK, FSS9, true, false);
   delay(1500);
 
   WiFi.begin(SSID, PASS);
 
   if (WiFi.status() != WL_CONNECTED){
 
-    displayLCDmessage("Failed To Connect To WiFi", tft.color565(70, 0, 0), FF25, true, true, 60);
-    displayLCDmessage("Retry:", tft.color565(0, 70, 70), FM9, true, false);
-    displayLCDmessage("Press (Top Left Button)", tft.color565(0, 70, 70), FM9, true, false);
-    displayLCDmessage("Return Home:", tft.color565(0, 70, 70), FM9, true, false);
-    displayLCDmessage("Press (Top Right Button)", tft.color565(0, 70, 70), FM9, true, false);
+    displayLCDmessage("Failed To Connect To WiFi", tft.color565(0, 0, 0), FSSB9, true, true, 60);
+    displayLCDmessage("Retry:", tft.color565(0, 0, 0), FSS9, true, false);
+    displayLCDmessage("Press (Top Left Button)", tft.color565(0, 0, 0), FSS9, true, false);
+    displayLCDmessage("Return Home:", tft.color565(0, 0, 0), FSS9, true, false);
+    displayLCDmessage("Press (Top Right Button)", tft.color565(0, 0, 0), FSS9, true, false);
     delete wioClient;
     while (true){
       if (digitalRead(BUTTON_3) == LOW){
@@ -265,7 +265,7 @@ void connectWifi(){
     }
   }
 
-  displayLCDmessage("WiFi Connected!", tft.color565(0, 110, 0), FF6, true, true, 110);
+  displayLCDmessage("WiFi Connected!", tft.color565(0, 0, 0), FSSB9, true, true, 110);
   delay(3000);
   wifiIsConnected = true;
 
@@ -281,26 +281,26 @@ void setupMqtt(){
 void connectMqtt(){
 
   doPub = false;
-  displayLCDmessage("(Re) Connecting to MQTT Broker...", tft.color565(70, 50, 100), FF25, true, true, 60);
-  displayLCDmessage("(Connection Keeps Failing? Fix:", TFT_BLACK, FF33, true, false);
-  displayLCDmessage("1. Broker Is Not Running)", TFT_BLACK, FF33, true, false);
-  displayLCDmessage("(Screen Static For T>10s? Fix:", TFT_BLACK, FF33, true, false);
-  displayLCDmessage("2. Faulty BrokerIP &&/|| WiFi Loss)", TFT_BLACK, FF33, true, false);
-  displayLCDmessage("Alt 2. Needs Manual Wio Restart", TFT_BLACK, FF33, true, false);
-  displayLCDmessage("After Reconfiguration", TFT_BLACK, FF33, true, false);
+  displayLCDmessage("(Re) Connecting to MQTT Broker...", tft.color565(0, 0, 0), FSSB9, true, true, 60);
+  displayLCDmessage("(Connection Keeps Failing? Fix:", TFT_BLACK, FSS9, true, false);
+  displayLCDmessage("1. Broker Is Not Running)", TFT_BLACK, FSS9, true, false);
+  displayLCDmessage("(Screen Static For T>10s? Fix:", TFT_BLACK, FSS9, true, false);
+  displayLCDmessage("2. Faulty BrokerIP &&/|| WiFi Loss)", TFT_BLACK, FSS9, true, false);
+  displayLCDmessage("Alt 2. Needs Manual Wio Restart", TFT_BLACK, FSS9, true, false);
+  displayLCDmessage("After Reconfiguration", TFT_BLACK, FSS9, true, false);
   delay(4000);
 
   if (mqttClient->connect(clientName)){
-    displayLCDmessage("Connected To MQTT Broker!", tft.color565(0, 110, 0), FS12, true, true, 110);
+    displayLCDmessage("Connected To MQTT Broker!", tft.color565(0, 0, 0), FSSB9, true, true, 110);
     brokerIsConnected = true;
     subscribeMqtt();
     delay (3000);
   } else {
-    displayLCDmessage("Failed To Connect To MQTT Broker", tft.color565(100, 0, 0), FSS9, true, true, 70);
-    displayLCDmessage("Retry:", tft.color565(0, 70, 70), FM9, true, false);
-    displayLCDmessage("Press (Top Left Button)", tft.color565(0, 70, 70), FM9, true, false);
-    displayLCDmessage("Return Home:", tft.color565(0, 70, 70), FM9, true, false);
-    displayLCDmessage("Press (Top Right Button)", tft.color565(0, 70, 70), FM9, true, false);
+    displayLCDmessage("Failed To Connect To MQTT Broker", tft.color565(0, 0, 0), FSSB9, true, true, 70);
+    displayLCDmessage("Retry:", tft.color565(0, 0, 0), FSS9, true, false);
+    displayLCDmessage("Press (Top Left Button)", tft.color565(0, 0, 0), FSS9, true, false);
+    displayLCDmessage("Return Home:", tft.color565(0, 0, 0), FSS9, true, false);
+    displayLCDmessage("Press (Top Right Button)", tft.color565(0, 0, 0), FSS9, true, false);
     delay(2000);
     while (true){
       if (digitalRead(BUTTON_3) == LOW){
@@ -349,7 +349,7 @@ void displayLCDmessage(char* message, uint16_t textColor, const GFXfont* font, b
 
   ::Y_Cord_Start_Pos = Y_Cord_Start_Pos;
   if (clearPrevLCD){
-    tft.fillScreen(TFT_WHITE);
+    tft.fillScreen(tft.color565(180,238,180));
     text_Y_Margin_Offset = 0;
   }
   tft.setTextColor(textColor);
